1c4277a55389979ee08fd52460ea6daf8d58f90b
Jun Shimamura <jsmmr@icloud.com>
2022-03-24 00:48:31 +0900

    Refactor example

--- examples/builder/internal-seekable.rb
+++ examples/builder/internal-seekable.rb
@@ -4,89 +4,85 @@ require 'callable_tree'
 require 'json'
 require 'rexml/document'
 
-module JSONParser
-  def self.build
-    CallableTree::Node::Internal::Builder
-      .new
-      .matcher do |input, **_options|
-        File.extname(input) == '.json'
-      end
-      .caller do |input, **options, &block|
-        File.open(input) do |file|
-          json = ::JSON.load(file)
-          # The following block call is equivalent to calling `super` in the class style.
-          block.call(json, **options)
-        end
-      end
-      .terminator do
-        true
-      end
-      .build
+JSONParser =
+  CallableTree::Node::Internal::Builder
+  .new
+  .matcher do |input, **_options|
+    File.extname(input) == '.json'
   end
+  .caller do |input, **options, &block|
+    File.open(input) do |file|
+      json = ::JSON.load(file)
+      # The following block call is equivalent to calling `super` in the class style.
+      block.call(json, **options)
+    end
+  end
+  .terminator do
+    true
+  end
+  .build
+
+XMLParser =
+  CallableTree::Node::Internal::Builder
+  .new
+  .matcher do |input, **_options|
+    File.extname(input) == '.xml'
+  end
+  .caller do |input, **options, &block|
+    File.open(input) do |file|
+      # The following block call is equivalent to calling `super` in the class style.
+      block.call(REXML::Document.new(file), **options)
+    end
+  end
+  .terminator do
+    true
+  end
+  .build
+
+def build_json_scraper(type)
+  CallableTree::Node::External::Builder
+    .new
+    .matcher do |input, **_options|
+      !!input[type.to_s]
+    end
+    .caller do |input, **_options|
+      input[type.to_s]
+        .map { |element| [element['name'], element['emoji']] }
+        .to_h
+    end
+    .build
 end
 
-module XMLParser
-  def self.build
-    CallableTree::Node::Internal::Builder
-      .new
-      .matcher do |input, **_options|
-        File.extname(input) == '.xml'
-      end
-      .caller do |input, **options, &block|
-        File.open(input) do |file|
-          # The following block call is equivalent to calling `super` in the class style.
-          block.call(REXML::Document.new(file), **options)
-        end
-      end
-      .terminator do
-        true
-      end
-      .build
-  end
+AnimalsJSONScraper = build_json_scraper(:animals)
+FruitsJSONScraper = build_json_scraper(:fruits)
+
+def build_xml_scraper(type)
+  CallableTree::Node::External::Builder
+    .new
+    .matcher do |input, **_options|
+      !input.get_elements("//#{type}").empty?
+    end
+    .caller do |input, **_options|
+      input
+        .get_elements("//#{type}")
+        .first
+        .map { |element| [element['name'], element['emoji']] }
+        .to_h
+    end
+    .build
 end
 
-module JSONScraper
-  def self.build(type)
-    CallableTree::Node::External::Builder
-      .new
-      .matcher do |input, **_options|
-        !!input[type.to_s]
-      end
-      .caller do |input, **_options|
-        input[type.to_s]
-          .map { |element| [element['name'], element['emoji']] }
-          .to_h
-      end
-      .build
-  end
-end
-
-module XMLScraper
-  def self.build(type)
-    CallableTree::Node::External::Builder
-      .new
-      .matcher do |input, **_options|
-        !input.get_elements("//#{type}").empty?
-      end
-      .caller do |input, **_options|
-        input
-          .get_elements("//#{type}")
-          .first
-          .map { |element| [element['name'], element['emoji']] }
-          .to_h
-      end
-      .build
-  end
-end
+AnimalsXMLScraper = build_xml_scraper(:animals)
+FruitsXMLScraper = build_xml_scraper(:fruits)
 
 tree = CallableTree::Node::Root.new.seekable.append(
-  JSONParser.build.new.seekable.append(
-    JSONScraper.build(:animals).new,
-    JSONScraper.build(:fruits).new
+  JSONParser.new.seekable.append(
+    AnimalsJSONScraper.new,
+    FruitsJSONScraper.new
   ),
-  XMLParser.build.new.seekable.append(
-    XMLScraper.build(:animals).new,
-    XMLScraper.build(:fruits).new
+  XMLParser.new.seekable.append(
+    AnimalsXMLScraper.new,
+    FruitsXMLScraper.new
   )
 )
 
